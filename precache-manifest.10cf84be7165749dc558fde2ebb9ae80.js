f91a015b6c494a5ebc5b3b510bc280ab16d3d786
Nikhil Singh Gautam <nick9124>
2020-08-10 02:58:42 +0530

    Updates

---
+++ precache-manifest.10cf84be7165749dc558fde2ebb9ae80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48044f7384f688968f5297744656aa9a",
+    "url": "/robo-monsters/index.html"
+  },
+  {
+    "revision": "85121f1cf1e11351c52a",
+    "url": "/robo-monsters/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fe0b0089d8e42e7f0d3b",
+    "url": "/robo-monsters/static/css/main.cd358663.chunk.css"
+  },
+  {
+    "revision": "85121f1cf1e11351c52a",
+    "url": "/robo-monsters/static/js/2.24fd1927.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo-monsters/static/js/2.24fd1927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0b0089d8e42e7f0d3b",
+    "url": "/robo-monsters/static/js/main.b57b0e00.chunk.js"
+  },
+  {
+    "revision": "9202dbf66fb99512e83f",
+    "url": "/robo-monsters/static/js/runtime-main.5f6497d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-monsters/static/media/SEGA.12a3db8b.woff"
+  }
+]);
